00061e6f378e7808d2ec1aa59dec85d18b6243b5
Luis Augusto Oliveira dos Santos <luisaugusto010203@gmail.com>
2023-12-13 15:39:32 -0300

    Primeiro Commit

---
+++ ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace ProjetoConta
+{
+    internal class ContaBancaria
+    {
+        private string _nome;
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(string nome, int numero)
+        {
+            _nome = nome;
+            Numero = numero;
+        }
+
+        public ContaBancaria(string nome, int numero, double depositoInicial) : this(nome, numero)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if(value != _nome && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public void Deposito(double saldo)
+        {
+            Saldo += saldo;
+        }
+
+        public void Saque(double saldo)
+        {
+            Saldo = Saldo - saldo - 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Nome + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoConta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            ContaBancaria conta = new ContaBancaria(nome, num);
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            double valor = 0;
+            char opc = char.Parse(Console.ReadLine());
+            if (opc == 's' || opc == 'S')
+            {
+
+                Console.Write("Entre o valor do depósito inicial: ");
+                valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(nome, num, valor);
+            }
+            else
+            {
+                conta = new ContaBancaria(nome, num);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+        }
+    }
+}
