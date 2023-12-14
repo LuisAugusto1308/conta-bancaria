c5f23e64b8943e311512be5b5da65c49653d5d41
Luis Augusto Oliveira dos Santos <luisaugusto010203@gmail.com>
2023-12-14 14:47:27 -0300

    Segundo Commit

--- Program.cs
+++ Program.cs
@@ -7,17 +7,17 @@ namespace ProjetoConta
     {
         static void Main(string[] args)
         {
+            ContaBancaria conta;
+
             Console.Write("Entre o número da conta: ");
             int num = int.Parse(Console.ReadLine());
             Console.Write("Entre o titular da conta: ");
             string nome = Console.ReadLine();
-            ContaBancaria conta = new ContaBancaria(nome, num);
             Console.Write("Haverá depósito inicial (s/n)? ");
             double valor = 0;
             char opc = char.Parse(Console.ReadLine());
             if (opc == 's' || opc == 'S')
             {
-
                 Console.Write("Entre o valor do depósito inicial: ");
                 valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 conta = new ContaBancaria(nome, num, valor);
